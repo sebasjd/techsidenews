98b9d0f3895e10ead1a1f83dfd78ca4abd2720a7
sebasjd <d.jsebastian@hotmail.com>
2023-02-09 00:53:44 -0300

    Change navigate 404

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -12,7 +12,7 @@ const NotFound = () => {
     <Container>
       <Number color={PrimaryDark} dsc={PrimaryStrong}>404</Number>
       <Error color={NoWhite}>Error 404: Page not found</Error>
-      <Back color={PrimaryStrong} fds={PrimaryLight} hfds={NoWhite} mbgc={PrimaryDark} onClick={ () => navigate(-1) } />
+      <Back color={PrimaryStrong} fds={PrimaryLight} hfds={NoWhite} mbgc={PrimaryDark} onClick={ () => navigate("/") } />
       <Error color={PrimaryLight}>Click to return</Error>
     </Container>
   )
