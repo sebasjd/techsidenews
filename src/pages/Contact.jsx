8b30f1977446c065b158ed683b0c03df877f109f
sebasjd <d.jsebastian@hotmail.com>
2023-02-17 00:36:26 -0300

    Added messenging system

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,54 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ColorsCtx } from '../styles/Colors'
+import { Button, ContactInput, Container, Label, TextArea } from '../styles/Inputs'
+import { useFormik } from 'formik'
+import axios from 'axios';
 
 const Contact = () => {
+
+  const { PrimaryStrong, NoWhite, PrimaryLight } = useContext(ColorsCtx)
+
+  const { values, handleChange, handleSubmit, handleReset } = useFormik({
+    initialValues: {
+      name:'',
+      phone: '',
+      email: '',
+      ref: '',
+      msg: ''
+    },
+    onSubmit: (values) => {
+      axios.post('https://api.telegram.org/bot6006558775:AAFAZzhndvLph-uzxf1M5ut8qK7WtV8Lqz8/sendMessage', 
+      {
+        chat_id: 5582297042,
+        text: `Nuevo mensaje del formulario:\n\nName:\n${values.name}\n\nPhone:\n${values.phone}\n\nEmail:\n${values.email}\n\nReference:\n${values.ref}\n\nMessage:\n${values.msg}`
+      })
+      .then(
+        handleReset()
+      )
+      .catch(error => {
+        console.error(error);
+      });
+    }
+  })
+
+
+
   return (
-    <div>Contact</div>
+    <>
+      <Container onSubmit={handleSubmit}>
+        <Label color2={PrimaryStrong}>Nombte y Apellido:</Label>
+        <ContactInput type='text' name='name' value={values.name} onChange={handleChange} bc={PrimaryStrong} bc2={PrimaryLight} color={NoWhite} color2={PrimaryStrong} />
+        <Label color2={PrimaryStrong}>Teléfono:</Label>
+        <ContactInput type='number' name='phone' value={values.phone} onChange={handleChange} bc={PrimaryStrong} bc2={PrimaryLight} color={NoWhite} color2={PrimaryStrong} />
+        <Label color2={PrimaryStrong}>Correo Electrónico:</Label>
+        <ContactInput  type='email' name='email' value={values.email} onChange={handleChange} bc={PrimaryStrong} bc2={PrimaryLight} color={NoWhite} color2={PrimaryStrong} />
+        <Label color2={PrimaryStrong}>Referencia:</Label>
+        <ContactInput  type='text' name='ref' value={values.ref} onChange={handleChange} bc={PrimaryStrong} bc2={PrimaryLight} color={NoWhite} color2={PrimaryStrong} />
+        <Label color2={PrimaryStrong}>Mensaje:</Label>
+        <TextArea  type='text' name='msg' value={values.msg} onChange={handleChange} bc={PrimaryStrong} bc2={PrimaryLight} color={NoWhite} color2={PrimaryStrong} />
+        <Button type='submit' onSubmit={handleSubmit} color={NoWhite} bgc={PrimaryStrong}> Enviar </Button>
+      </Container>
+    </>
   )
 }
 
---
+++ src/styles/Inputs.js
@@ -0,0 +1,77 @@
+import styled from "styled-components";
+
+
+export const Container = styled.form`
+position: relative;
+flex-direction: column;
+top: 100px;
+width: 100vw;
+height: auto;
+display: flex;
+justify-content: center;
+align-items: center;
+`
+export const ContactInput = styled.input`
+  display: flex;
+  position: relative;
+  border: inset 1px ${props=>props.bc};
+  outline: none;
+  background-color: transparent;
+  height: 25px;
+  width: 85%;
+  max-width: 350px;
+  color: ${props=>props.color2};
+  font-size: 18px;
+  text-align: start;
+  padding: 5px 10px;
+  margin-bottom: 10px;
+  letter-spacing: 2px;
+  transition: all .3s;
+
+&:focus{
+  color: ${props=>props.color};
+  font-size: 20px;  border: outset 1px ${props=>props.bc2};
+}
+`
+
+export const TextArea = styled.textarea`
+display: flex;
+position: relative;
+border: inset 1px ${props=>props.bc};
+outline: none;
+background-color: transparent;
+height: 250px;
+width: 85%;
+padding: 5px 10px;
+max-width: 350px;
+color: ${props=>props.color2};
+font-size: 20px;
+text-align: start;
+white-space: pre-line;
+letter-spacing: 2px;
+overflow-y: visible;
+transition: all .3s;
+
+&:focus{
+  color: ${props=>props.color};
+  font-size: 20px;  border: outset 1px ${props=>props.bc2};
+}
+`
+
+export const Label = styled.label`
+display: flex;
+position: relative;
+color: ${props=>props.color2};
+font-size: 20px;
+text-align: left;
+width: 85%;
+max-width: 350px;
+letter-spacing: 2px;
+`
+
+export const Button = styled.button`
+  background-color: ${props=>props.bgc};
+  color: ${props=>props.color};
+  width: 100px;
+  height: 70px;
+`
