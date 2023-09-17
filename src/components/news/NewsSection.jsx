8a7501bf7900cc19275d163d2d32d8e66ccfe319
sebasjd <d.jsebastian@hotmail.com>
2023-09-17 16:59:07 -0400

    .

--- src/components/news/NewsSection.jsx
+++ src/components/news/NewsSection.jsx
@@ -24,9 +24,9 @@ const NewsSection = ( ) => {
                 img={e.img}
               />
               {(index > 0 && (index % 5 === 0)) && <Advertisement
-                key={Advertisements[(index+1)/6].id}
-                img={Advertisements[(index+1)/6].imgLarge} 
-                imgShort={Advertisements[(index+1)/6].imgShort} 
+                key={Advertisements[(index)/6].id}
+                img={Advertisements[(index)/6].imgLarge} 
+                imgShort={Advertisements[(index)/6].imgShort} 
               />}
             </React.Fragment> 
           )}
